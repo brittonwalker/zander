829980725689db74036478cc1211217b2065d015
Britton Walker <bwalker1801@gmail.com>
2021-01-31 22:29:59 -0500

    Adjust homepage animation

--- wp-content/themes/zander/js/app/HomePage.js
+++ wp-content/themes/zander/js/app/HomePage.js
@@ -4,7 +4,8 @@
 
 import $ from 'jquery';
 import {
-    gsap
+    gsap,
+    Power3
 } from 'gsap';
 import Typewriter from 'typewriter-effect/dist/core';
 
@@ -19,11 +20,11 @@ class Homepage {
         $(document).ready(() => {
             setTimeout( () => {
                 var tl = gsap.timeline();
-                tl.fromTo('.passport-information', { opacity: 0, y: 15 }, { opacity: 1, y: 0} );
-                tl.fromTo('.biography', { opacity: 0, y: 15 }, { opacity: 1, y: 0} );
-                tl.to('.image-overlay', { width: 0 }, 1);
-                tl.fromTo('.portrait-stamp', { scale: 3, opacity: 0, rotate: '45deg' }, { scale: 1, opacity: 1, rotate: 0 } );
-            }, 1000)
+                // tl.fromTo('.passport-information', { opacity: 0, y: 15 }, { opacity: 1, y: 0} );
+                // tl.fromTo('.biography', { opacity: 0, y: 15 }, { opacity: 1, y: 0} );
+                // tl.to('.image-overlay', { width: 0 }, 1);
+                tl.fromTo('.portrait-stamp', { scale: 3, opacity: 0, right: 20, y: -20, rotate: '-45deg' }, { scale: 1, right: 0, y: 0, opacity: 1, rotate: 0, duration: 1, ease: Power3.easeOut } );
+            }, 600)
         });
 
     }
