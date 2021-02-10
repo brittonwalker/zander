65ca3e14a89808f656354763ead56b2f680adc20
Britton Walker <bwalker1801@gmail.com>
2021-02-09 21:48:46 -0500

    V1 tweaks

--- wp-content/themes/zander/js/app/HomePage.js
+++ wp-content/themes/zander/js/app/HomePage.js
@@ -23,7 +23,8 @@ class Homepage {
                 // tl.fromTo('.passport-information', { opacity: 0, y: 15 }, { opacity: 1, y: 0} );
                 // tl.fromTo('.biography', { opacity: 0, y: 15 }, { opacity: 1, y: 0} );
                 // tl.to('.image-overlay', { width: 0 }, 1);
-                tl.fromTo('.portrait-stamp', { scale: 3, opacity: 0, right: 20, y: -20, rotate: '-45deg' }, { scale: 1, right: 0, y: 0, opacity: 1, rotate: 0, duration: 1, ease: Power3.easeOut } );
+                tl.fromTo('.portrait-stamp', { scale: 2, opacity: 0, rotate: '-45deg' }, { scale: 1, opacity: 1, duration: .5, ease: Power3.easeOut } );
+                tl.to('.portrait-stamp', { scale: 1, rotate: 0, duration: 1, ease: Power3.easeOut } );
             }, 600)
         });
 
