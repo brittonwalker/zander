912c5f37fcfd5d991c3ccfcbdae00c5cf3ddc29a
Britton Walker <bwalker1801@gmail.com>
2021-02-20 15:50:00 -0500

    Rotate seal

--- wp-content/themes/zander/js/app/HomePage.js
+++ wp-content/themes/zander/js/app/HomePage.js
@@ -7,7 +7,6 @@ import {
     gsap,
     Power3
 } from 'gsap';
-import Typewriter from 'typewriter-effect/dist/core';
 
 class Homepage {
 
@@ -19,15 +18,16 @@ class Homepage {
 
         $(document).ready(() => {
             setTimeout( () => {
-                var tl = gsap.timeline();
-                // tl.fromTo('.passport-information', { opacity: 0, y: 15 }, { opacity: 1, y: 0} );
-                // tl.fromTo('.biography', { opacity: 0, y: 15 }, { opacity: 1, y: 0} );
-                // tl.to('.image-overlay', { width: 0 }, 1);
-                tl.fromTo('.portrait-stamp', { scale: 2, opacity: 0, rotate: '-45deg' }, { scale: 1, opacity: 1, duration: .5, ease: Power3.easeOut } );
-                tl.to('.portrait-stamp', { scale: 1, rotate: 0, duration: 1, ease: Power3.easeOut } );
+                gsap.fromTo('.portrait-stamp', { scale: 2, opacity: 0, rotate: '-45deg' }, { scale: 1, opacity: 1, duration: .5, ease: Power3.easeOut, onComplete: this.rotate } );
             }, 600)
         });
 
+    }
+
+    rotate() {
+        let stamp = document.querySelector('.portrait-stamp').children[0];
+        stamp.classList.add('animate-spin');
+        stamp.style.animationDuration = '4s';
     }
 
 }
