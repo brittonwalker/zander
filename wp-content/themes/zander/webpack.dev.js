663467c671b3f94a57bfa8b9ffbb32da5a699512
Britton Walker <bwalker1801@gmail.com>
2021-08-14 15:01:06 -0400

    Use progress bar in webpack

--- wp-content/themes/zander/webpack.dev.js
+++ wp-content/themes/zander/webpack.dev.js
@@ -1,20 +1,31 @@
-const { merge } = require('webpack-merge');
-const {themePath, ...common} = require('./webpack.common.js');
+const {
+    merge
+} = require('webpack-merge');
+const {
+    themePath,
+    ...common
+} = require('./webpack.common.js');
 const path = require('path');
 const MiniCssExtractPlugin = require("mini-css-extract-plugin");
 const LiveReloadPlugin = require('webpack-livereload-plugin');
+const ProgressBarPlugin = require('progress-bar-webpack-plugin');
+const chalk = require('chalk');
 
 module.exports = merge(common, {
-  mode: 'development',
-  devtool: 'inline-source-map',
-  plugins: [
-    new MiniCssExtractPlugin({
-      filename: '../css/[name].css',
-    }),
-    new LiveReloadPlugin(),
-  ],
-  output: {
-    filename: '[name].js',
-    path: path.resolve(__dirname, `${themePath}/assets/js/`),
-  },
+    mode: 'development',
+    devtool: 'inline-source-map',
+    plugins: [
+        new MiniCssExtractPlugin({
+            filename: '../css/[name].css',
+        }),
+        new LiveReloadPlugin(),
+        new ProgressBarPlugin({
+            format: '  build [:bar] ' + chalk.green.bold(':percent') + ' (:elapsed seconds)',
+            clear: false
+        })
+    ],
+    output: {
+        filename: '[name].js',
+        path: path.resolve(__dirname, `${themePath}/assets/js/`),
+    },
 });
