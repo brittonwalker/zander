003a8dd69db7b04777251fdac6cf281a55b7362a
Britton Walker <bwalker1801@gmail.com>
2021-08-24 21:40:35 -0400

    Fix seal rotating

--- wp-content/themes/zander/js/app/HomePage.js
+++ wp-content/themes/zander/js/app/HomePage.js
@@ -5,25 +5,60 @@
 import $ from 'jquery';
 import {
     gsap,
-    Power3
+    Linear,
+    Power3,
+    TweenMax
 } from 'gsap';
 
 class Homepage {
 
     constructor() {
 
-        if ( !$('.home-wrapper').length ) {
+        if (!$('.home-wrapper').length) {
             return;
         }
 
-        $(document).ready(() => {
-            setTimeout( () => {
-                var tl = gsap.timeline();
-                tl.fromTo('.portrait-stamp', { scale: 2, opacity: 0, rotate: '-45deg' }, { scale: 1, opacity: 1, duration: .5, ease: Power3.easeOut } );
-                tl.to('.portrait-stamp', { scale: 1, rotate: 0, duration: 1, ease: Power3.easeOut } );
-            }, 600)
+        document.addEventListener("DOMContentLoaded", () => {
+            this.init();
         });
 
+    }
+
+    rotateStamp() {
+        return TweenMax.to($('.portrait-stamp'), 15, {
+            css: {
+                rotation: 360
+            },
+            // remove default easing for seamless loop
+            ease: Linear.easeNone,
+            repeat: -1,
+            paused: false
+        });
+    }
+
+    init() {
+        setTimeout(() => {
+            var tl = gsap.timeline({
+                onComplete: () => { this.rotateStamp() }
+            });
+            tl.fromTo('.portrait-stamp', {
+                scale: 2,
+                opacity: 0,
+                rotate: '-45deg'
+            }, {
+                scale: 1,
+                opacity: 1,
+                duration: .5,
+                ease: Power3.easeOut
+            });
+            tl.to('.portrait-stamp', {
+                scale: 1,
+                rotate: 0,
+                duration: -1,
+                ease: Power3.easeOut,
+                onComplete: () => console.log('hello')
+            });
+        }, 600)
     }
 
 }
